5d58b4c4d43622288d3117431d45da9a4cb081e7
kakaomame <nattyannkunndesu@icloud.com>
2025-10-07 22:41:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -65,9 +65,11 @@ HOMEHTML = """
                 <li><a href="/home">ホーム</a></li>
                 <li><a href="/h">GITHUBにセーブデータ保存</a></li>
                 <li><a href="/cmd">Webコマンド実行ツール</a></li>
+                <br>
                 <li><a href="/run?cmd=">直接コマンド実行したい方向け...</a></li>
                 <li><a href="/link">URL検索✨</a></li>
                 <li><a href="/url-dl">オンラインダウンローダー</a></li>
+                <br>
                 <li><a href="/games">ゲーム👿</a></li>
                 
             </ul>
